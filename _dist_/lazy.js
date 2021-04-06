f393bb1e963613350d79677e2d9890327af3d9b2
MauricioMorenoMorales <MauricioMorenoMorales@users.noreply.github.com>
2021-04-06 22:36:20 +0000

    deploy: cc16a0be2d645da6d1ca2b687a463ca4a162b5b0

--- _dist_/lazy.js
+++ _dist_/lazy.js
@@ -3,16 +3,18 @@ const isIntersecting = entry => {
 	return entry.isIntersecting // true dentro de la pantalla o falso
 }
 
-const accion = entry => {
-	const nodo = entry.target
-	console.log('Holis')
-
+const loadImage = entry => {
+	const container = entry.target // container (DIV)
+	const image = container.firstChild
+	const url = image.dataset.src
+	//LOad image
+	image.src = url
 	//des registra la imagen (unlisten)
-	observer.unobserve(nodo)
+	observer.unobserve(container)
 }
 
 const observer = new IntersectionObserver(entries => {
-	entries.filter(isIntersecting).forEach(accion)
+	entries.filter(isIntersecting).forEach(loadImage)
 })
 //
 export const registerImage = imagen => {
